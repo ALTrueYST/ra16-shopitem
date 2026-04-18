8e73d1c96fe8fbdc58641af7129f2085c65f1982
ALTrueYST <altrueyst@mail.ru>
2026-04-18 15:00:17 +0300

    1 try

---
+++ src/App.jsx
@@ -0,0 +1,28 @@
+import { FuncShop } from './components/fc/FuncShop'
+import './Apps.css'
+
+function App() {
+  return (
+    <div className="container">
+      <div className="background-element">
+      </div>
+      <div className="highlight-window">
+        <div className='highlight-overlay'></div>
+      </div>
+      <div className="window">
+      <FuncShop item={item} />
+      </div>
+    </div>
+  )
+}
+
+const item = {
+  brand: 'Tiger of Sweden',
+  title: 'Leonard coat',
+  description: 'Minimalistic coat in cotton-blend',
+  descriptionFull: 'Men\'s minimalistic overcoat in cotton-blend. Features a stand-up collar, concealed front closure and single back vent. Slim fit with clean, straight shape. Above-knee length.',
+  price: 399,
+  currency: '£'
+}
+
+export default App
---
+++ src/components/fc/FuncShop.jsx
@@ -0,0 +1,20 @@
+export const FuncShop = ( props ) => {
+  const { item } = props;
+
+  return (
+    <div className="main-content">
+      <h2>{item.brand}</h2>
+      <h1>{item.title}</h1>
+      <h3>{item.description}</h3>
+      <div>
+        {item.descriptionFull}
+      </div>
+      <div className="highlight-window mobile"><div className="highlight-overlay"></div></div>
+      <div class="divider"></div>
+      <div class="purchase-info">
+        <div class="price">{item.currency}{item.price}</div>
+        <button>Add to cart</button>
+      </div>
+    </div>
+  )
+}
